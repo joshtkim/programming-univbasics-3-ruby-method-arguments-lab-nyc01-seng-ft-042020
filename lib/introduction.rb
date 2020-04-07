09857caeb3b94b0a7933ec14b9f0e2ee3a62b97d
Joshua Kim <jykim92@gmail.com>
2020-04-07 21:00:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ end
 
 introduction("Josh")
 
-def introduction_with_langauge(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
