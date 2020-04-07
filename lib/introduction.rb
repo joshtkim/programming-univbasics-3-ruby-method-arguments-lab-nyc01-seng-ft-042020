5a05c70dfd5814dd08478286cfcd77ab47906cc4
Joshua Kim <jykim92@gmail.com>
2020-04-07 20:59:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,17 +7,19 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction("Josh")
 
 def introduction_with_langauge(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction_with_language("Josh", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+introduction_with_language_optional("Josh", "Python")
 
 
 
